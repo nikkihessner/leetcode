a4c9b9db957f0443ffe10a08a8ae83bc022a13db
Nikki Hessner <nikki@nikkihessner.com>
2025-05-02 22:20:43 -0700

    Finalize dominoes simulation with bitmask logic, dual force pass, and documentation

--- daily/05-01-2025/pushDominoes.py
+++ daily/05-01-2025/pushDominoes.py
@@ -1,51 +1,15 @@
-"""838. Push Dominoes
-Medium
-
-Topics
-Companies
-There are n dominoes in a line, and we place each domino vertically upright. In the beginning, we simultaneously push some of the dominoes either to the left or to the right.
-
-After each second, each domino that is falling to the left pushes the adjacent domino on the left. Similarly, the dominoes falling to the right push their adjacent dominoes standing on the right.
-
-When a vertical domino has dominoes falling on it from both sides, it stays still due to the balance of the forces.
-
-For the purposes of this question, we will consider that a falling domino expends no additional force to a falling or already fallen domino.
-
-You are given a string dominoes representing the initial state where:
-
-dominoes[i] = 'L', if the ith domino has been pushed to the left,
-dominoes[i] = 'R', if the ith domino has been pushed to the right, and
-dominoes[i] = '.', if the ith domino has not been pushed.
-Return a string representing the final state.
-
- 
-
-Example 1:
-
-Input: dominoes = "RR.L"
-Output: "RR.L"
-Explanation: The first domino expends no additional force on the second domino.
-Example 2:
-
-
-Input: dominoes = ".L.R...LR..L.."
-Output: "LL.RR.LLRRLL.."
- 
-
-Constraints:
-
-n == dominoes.length
-1 <= n <= 105
-dominoes[i] is either 'L', 'R', or '.'."""
 class Solution:
     # need to check the rightmost domino for falling left and leftmost domino for falling right and then iterate in opposite directions
+    @staticmethod
     def pushDominoes(dominoes: str) -> str:
         # convert dominoes to a list since str is an immutable type
         n = len(dominoes)
 
+        # don't eat time and resources when you don't need to
         if n == 1:
             return dominoes 
-
+        
+        # make dominoes mutable
         dominoes = list(dominoes)
         
         # bitmask the dominoes to reduce memory usage
@@ -56,40 +20,84 @@ class Solution:
         }
         dominoes = [bitmask[d] for d in dominoes]
 
-        # make copies of dominoes to examine leftward and rightward forces separately
+        # make copies of dominoes to examine leftward and rightward forces separately and initialize
         right = [dominoes[0]]
         left = [dominoes[-1]]
 
+        # analyze the dominoes falling from left to right
+        count = 0
         for i in range(n - 2):
+            # check to see if the domino to the right is either a right domino or an upright domino
+            # if the domino two to the right is falling left, it will create a condition for an upright domino
+            # the last domino in right is the one acting on the rest.
             if dominoes[i + 1] in [0b00, 0b01] and dominoes[i + 2] != 0b10 and right[-1] == 0b01:
                 right.append(0b01)
+                # the count only counts upright dominos that are affected, so they can be divided between R and L dominos
+                if dominoes[i + 1] == 0b00:
+                    count +=1
+                else:
+                    count = 0 
+            # accounts for upright dominoes with no forces acting on them
             elif dominoes[i + 1] == 0b00 and right[-1] == 0b00:
                 right.append(0b00)
+            # accounts for the division of long strings of upright dominoes between an R and a L being split evenly between them
             else:
                 right.append(dominoes[i+1])
+                if count > 2 and right[-1] != 0b01:
+                    shift = count >> 1
+                    right[-shift:] = [0b00] * shift
+
+                    if count & 1:
+                        right[-shift-1:] = [0b00] * shift
+                        right.append(dominoes[i+1])
+                    else:
+                        right[-shift:] = [0b00] * shift
+                
+                count = 0
         
-        if dominoes[-2] == 0b01 and dominoes[-1] != 0b10:
+        # handle the last domino, since (n-2)
+        if right[-1] == 0b01 and dominoes[-1] != 0b10:
             right.append(0b01)
         else:
             right.append(dominoes[-1])
 
+        # same as above, but for left, running through the data right to left
+        count = 0
         for i in range(n - 2):
             if dominoes[-2 - i] in [0b00, 0b10] and dominoes[-3 - i] != 0b01 and left[-1] == 0b10:
                 left.append(0b10)
+                if dominoes[-2 -i] == 0b00:
+                    count += 1
+                else:
+                    count = 0
             elif dominoes[-2 - i] == 0b00 and left[-1] == 0b00:
                 left.append(0b00)
             else:
                 left.append(dominoes[-2 - i])
+                if count > 2 and left[-1] != 0b10:
+                    shift = count >> 1
+                    left[-shift:] = [0b00] * shift
+
+                    if count & 1:
+                        left[-shift-1:] = [0b00] * shift
+                        left.append(dominoes[-2 - i])
+                    else:
+                        left[-shift:] = [0b00] * shift
+                
+                count = 0
         
-        if dominoes[1] == 0b10 and dominoes[0] != 0b01:
+        if left[-1] == 0b10 and dominoes[0] != 0b01:
             left.append(0b10)
         else:
             left.append(dominoes[0])
 
+        # reverse
         left.reverse()        
 
+        # compare the two lists with bitwise or to determine which forces are acting on the upright dominoes
         dominoes = [r | l for r, l in zip(right, left)]
 
+        # reverse the bitmask
         reverse_bitmask = {0b00: '.',
                            0b01: 'R',
                            0b10: 'L',
@@ -97,8 +105,7 @@ class Solution:
         
         dominoes = [reverse_bitmask[d] for d in dominoes]
 
-
-
+        # convert back to string
         dominoes = ''.join(dominoes)
 
         return(dominoes)
--- daily/05-01-2025/test_pushDominoes.py
+++ daily/05-01-2025/test_pushDominoes.py
@@ -24,3 +24,20 @@ def test_pushDominoes_case7():
 def test_pushDominoes_case8():
     assert Solution.pushDominoes(dominoes="L.") == "L."
 
+def test_pushDominoes_case9():
+    assert Solution.pushDominoes(dominoes="..R..") == "..RRR"
+
+def test_pushDominoes_case10():
+    assert Solution.pushDominoes(dominoes="R.......L.R.........") == "RRRR.LLLL.RRRRRRRRRR"
+
+def test_pushDominoes_case11():
+    assert Solution.pushDominoes(dominoes="RR..LL") == "RRRLLL"
+
+def test_pushDominoes_case12():
+    assert Solution.pushDominoes(dominoes="L.....RR.RL.....L.R.") == "L.....RRRRLLLLLLL.RR"
+
+def test_pushDominoes_case13():
+    assert Solution.pushDominoes(dominoes="...RL....R.L.L........RR......L....R.L.....R.L..RL....R....R......R.......................LR.R..L.R.") == "...RL....R.LLL........RRRRRLLLL....R.L.....R.L..RL....RRRRRRRRRRRRRRRRRRRRRRRR.LLLLLLLLLLLLRRRRLL.RR" 
+
+def test_pushDominoes_case14():
+    assert Solution.pushDominoes(dominoes="RLLL..LR....LL......LLR.RL...RRL..........R..L....RR.R..L.LR.L...L..LL.R.R.L.RR.....LRL.L.LL..LRR.L.") == "RLLLLLLRRRLLLLLLLLLLLLRRRL...RRL..........RRLL....RRRRRLLLLR.LLLLLLLLL.RRR.L.RRRR.LLLRLLLLLLLLLRR.L."
